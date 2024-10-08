09972e1eca49685afde70b22ff605119058851b0
RohanManojPatil <144838652+RohanManojPatil@users.noreply.github.com>
2024-10-08 23:38:16 +0530

    Add files via upload

---
+++ October-2024/08-10-2024.cpp
@@ -0,0 +1,27 @@
+
+*******************************LEETCODE POTD*************************
+*	               T.C = O(n)							                * 
+*	               							                        * 
+*	               S.C = O(1)                                          * 
+*	                                                                * 
+*********************************************************************
+class Solution {
+public:
+    int minSwaps(string s) {
+        int size = 0;
+
+        for(int i = 0; i<s.length(); i++)
+        {
+            if(s[i] == '[')
+            {
+                size++;
+            }
+            else if(size>0)
+            {
+                size--;
+            }
+        }
+
+        return (size+1)/2;
+    }
+};
