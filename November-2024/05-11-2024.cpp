e77a8d84f5f1f308823c5f71e21a23bf7a75fc43
Rohan Patil <144838652+RohanManojPatil@users.noreply.github.com>
2024-11-06 01:22:46 +0530

    Add files via upload

---
+++ November-2024/05-11-2024.cpp
@@ -0,0 +1,20 @@
+*******************************LEETCODE POTD*************************
+*	               T.C = O(N)							                * 
+*	               							                        * 
+*	               S.C = O(1)                                           * 
+*	                                                                * 
+*********************************************************************
+class Solution {
+public:
+    int minChanges(const std::string& s) {
+    int count = 0;
+    for (int i = 0; i < s.size() - 1; i += 2) {
+        if (s[i] != s[i + 1]) {
+            count++;
+        }
+    }
+    return count;
+}
+};
+
+
