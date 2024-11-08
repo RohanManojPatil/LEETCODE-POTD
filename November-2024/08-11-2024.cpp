8f529681cf7acf9117961e6b01d47d496449568a
Rohan Patil <144838652+RohanManojPatil@users.noreply.github.com>
2024-11-08 18:06:20 +0530

    Add files via upload

---
+++ November-2024/08-11-2024.cpp
@@ -0,0 +1,20 @@
+
+*******************************LEETCODE POTD*************************
+*	               T.C = O(N)							                * 
+*	               							                        * 
+*	               S.C = O(N)                                          * 
+*	                                                                * 
+*********************************************************************
+class Solution {
+public:
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        int n = nums.size(),xorr = nums[0],maxxorr = pow(2,maximumBit)-1;
+        for(int i=1;i<n;i++)xorr ^= nums[i];
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            ans[i] = xorr^maxxorr;
+            xorr ^= nums[n-1-i];
+        }
+        return ans;
+    }
+};
