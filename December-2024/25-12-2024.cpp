4b6ab32929096852fa13a9f98048fde4af3a594b
Rohan Patil <144838652+RohanManojPatil@users.noreply.github.com>
2024-12-25 11:18:31 +0530

    Add files via upload

---
+++ December-2024/25-12-2024.cpp
@@ -0,0 +1,57 @@
+*******************************LEETCODE POTD*************************
+*	               T.C = O(N)							                * 
+*	               							                        * 
+*	               S.C = O(N)                                           * 
+*	                                                                * 
+*********************************************************************
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        if(!root)
+        {
+            return {};
+        }
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        vector<int> ans;
+
+        while(!q.empty())
+        {
+            int size = q.size();
+            int maxi = INT_MIN;
+
+            while(size--)
+            {
+                TreeNode* node = q.front();
+                q.pop();
+
+                maxi = max(maxi, node->val);
+
+                if(node->left)
+                {
+                    q.push(node->left);
+                }
+                if(node->right)
+                {
+                    q.push(node->right);
+                }
+            }
+
+            ans.push_back(maxi);
+        }
+
+        return ans;
+    }
+};
